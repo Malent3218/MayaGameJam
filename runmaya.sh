1870f35b1b0c5e4e9cacc6343c043187d392398a
Timothy So <so.timothy.p@gmail.com>
2016-11-30 11:38:13 +0000

    Added Windows behavior for runmaya.sh

--- runmaya.sh
+++ runmaya.sh
@@ -1,5 +1,13 @@
-export PYTHONPATH=$PYTHONPATH:/Users/timothyso/Projects/MayaGameJam
-export MAYA_SCRIPT_PATH=$MAYA_SCRIPT_PATH:/Users/timothyso/Projects/MayaGameJam
-export XBMLANGPATH=$XBMLANGPATH:/Users/timothyso/Projects/MayaGameJam
-
-open /Applications/Autodesk/maya2016/Maya.app/
+WINNAME="cygwin"
+if [ "$OSTYPE" = "$WINNAME" ]
+then
+    export PYTHONPATH="$PYTHONPATH;C:\\Users\\Tim\\Projects\\MayaGameJam"
+    export MAYA_SCRIPT_PATH="$MAYA_SCRIPT_PATH;C:\\Users\\Tim\\Projects\\MayaGameJam"
+    export XBMLANGPATH="$XBMLANGPATH;C:\\Users\\Tim\\Projects\\MayaGameJam"
+    `OS=""; TMP=""; TEMP=""; cygstart "C:\\Program Files\\Autodesk\\Maya2017\\bin\\maya.exe"`
+else
+    export PYTHONPATH=$PYTHONPATH:/Users/timothyso/Projects/MayaGameJam
+    export MAYA_SCRIPT_PATH=$MAYA_SCRIPT_PATH:/Users/timothyso/Projects/MayaGameJam
+    export XBMLANGPATH=$XBMLANGPATH:/Users/timothyso/Projects/MayaGameJam
+    `open /Applications/Autodesk/maya2016/Maya.app/`
+fi
