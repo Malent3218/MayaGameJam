69e86aca2fd6951195aed1ad19f9ea7534f3ec94
Timothy So <sot@usc.edu>
2016-11-30 21:43:42 -0800

    Upgrade Mac to Maya2017

--- runmaya.sh
+++ runmaya.sh
@@ -9,5 +9,5 @@ else
     export PYTHONPATH=$PYTHONPATH:/Users/timothyso/Projects/MayaGameJam
     export MAYA_SCRIPT_PATH=$MAYA_SCRIPT_PATH:/Users/timothyso/Projects/MayaGameJam
     export XBMLANGPATH=$XBMLANGPATH:/Users/timothyso/Projects/MayaGameJam
-    `open /Applications/Autodesk/maya2016/Maya.app/`
+    `open /Applications/Autodesk/maya2017/Maya.app/`
 fi
